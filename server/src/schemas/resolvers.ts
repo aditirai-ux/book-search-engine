3f8636a51c5f60a255d592ea0510d34debf668c7
Aditi Rai <aditi45@gmail.com>
2025-02-06 17:11:12 -0500

    updated interfaces in resolvers file

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,16 +1,41 @@
 import User from '../models/index.js';
-import { AuthenticationError } from '../utils/auth.js';
+import { signToken, AuthenticationError } from '../utils/auth.js';
 
 interface User {
-    id: string;
+    username: string;
+    email: string;
+    password: string;
+    bookCount: number;
+}
+
+interface userArgs {
+    _id: string;
+}
+
+interface AddUserArgs {
+    username: string;
     email: string;
     password: string;
 }
 
-interface Book {}
+interface saveBookArgs {
+    bookData: {
+        authors: string[];
+        description: string;
+        title: string;
+        bookId: string;
+        image: string;
+        link: string;
+    }
+}
 
-interface UserInput {}
-interface BookInput {}
+interface removeBookArgs {
+    bookId: string;
+}
+
+interface Context {
+    user?: User;
+}
 
 const resolvers = {
     Query: {
